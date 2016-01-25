37a730be7a332037ce487a31c6f031f769a4304f
yuemori <yuemori@aiming-inc.com>
2016-01-25 23:42:06 +0900

    cosme

--- spec/bq_factory_spec.rb
+++ spec/bq_factory_spec.rb
@@ -1,11 +1,4 @@
 require 'spec_helper'
 
 describe BqFactory do
-  it 'has a version number' do
-    expect(BqFactory::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
