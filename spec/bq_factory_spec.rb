a0e45203d8c478ade6723c7e93634fe8d0c7adf5
yuemori <yuemori@aiming-inc.com>
2016-01-30 14:02:30 +0900

    Add spec

--- spec/bq_factory_spec.rb
+++ spec/bq_factory_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe BqFactory do
+  describe '.configuration' do
+    subject { described_class.configuration }
+    it { is_expected.to be_instance_of BqFactory::Configuration }
+  end
+
+  describe '.configure' do
+    subject { described_class.configure }
+    it { is_expected.to be_instance_of BqFactory::Configuration }
+  end
 end
