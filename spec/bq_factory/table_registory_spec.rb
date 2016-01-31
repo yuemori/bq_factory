1fc10b9beb816b31537bb55d5147afdf545c72da
yuemori <yuemori@aiming-inc.com>
2016-01-31 22:18:20 +0900

    Refactor TableRegistory spec

--- spec/bq_factory/table_registory_spec.rb
+++ spec/bq_factory/table_registory_spec.rb
@@ -1,18 +1,20 @@
 require 'spec_helper'
 
 describe BqFactory::TableRegistory do
+  let(:table_registory) { described_class.new }
+
   describe '#register' do
-    subject { described_class.new.register(name, table) }
+    subject { table_registory.register(name, table) }
     let(:table) { double('Table') }
     let(:name)  { :dummy_table }
 
     context 'when not registered table' do
-      before { allow_any_instance_of(described_class).to receive(:registered?).and_return(false) }
+      before { allow(table_registory).to receive(:registered?).and_return(false) }
       it { expect { subject }.not_to raise_error }
     end
 
     context 'when registered table' do
-      before { allow_any_instance_of(described_class).to receive(:registered?).and_return(true) }
+      before { allow(table_registory).to receive(:registered?).and_return(true) }
       it { expect { subject }.to raise_error BqFactory::DuplicateDefinitionError }
     end
   end
