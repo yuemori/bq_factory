1a689f54d37b4210e4d15051c198d5205d7bb549
yuemori <yuemori@aiming-inc.com>
2016-01-31 14:04:48 +0900

    Implement DSL#factory

--- lib/bq_factory/dsl.rb
+++ lib/bq_factory/dsl.rb
@@ -3,5 +3,11 @@ module BqFactory
     def self.run(&block)
       new.instance_eval(&block)
     end
+
+    def factory(name, options = {})
+      table_name = options.key?(:reference) ? options[:reference] : name
+      table = BqFactory.client.table(table_name)
+      BqFactory.register_table(table)
+    end
   end
 end
--- spec/bq_factory/dsl_spec.rb
+++ spec/bq_factory/dsl_spec.rb
@@ -9,4 +9,33 @@ describe BqFactory::DSL do
       expect { subject }.not_to raise_error
     end
   end
+
+  describe '#factory' do
+    subject { described_class.new.factory(name, options) }
+
+    let(:table)   { double('Table') }
+    let(:name)    { :dummy_table }
+    let(:options) { Hash.new }
+
+    shared_examples_for 'fetch table from bigquery' do
+      it 'should get expect table' do
+        expect(BqFactory).to receive(:register_table).with(table)
+        expect_any_instance_of(BqFactory::Client).to receive(:table).with(expect_table_name).and_return(table)
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context 'when not specify table name' do
+      let(:expect_table_name) { name }
+
+      it_behaves_like 'fetch table from bigquery'
+    end
+
+    context 'when specify table name in options' do
+      let(:expect_table_name) { options[:reference] }
+      let(:options) { { reference: :reference_table } }
+
+      it_behaves_like 'fetch table from bigquery'
+    end
+  end
 end
