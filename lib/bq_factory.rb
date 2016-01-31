27437e17169d150b6cbba235f169cfeff9db72de
yuemori <yuemori@aiming-inc.com>
2016-01-31 22:06:31 +0900

    Refactor BqFactory

--- lib/bq_factory.rb
+++ lib/bq_factory.rb
@@ -7,41 +7,43 @@ require "bq_factory/registory"
 require "bq_factory/table_registory"
 
 module BqFactory
-  def self.configure
-    yield configuration if block_given?
-    configuration
-  end
-
-  def self.configuration
-    @configuration ||= Configuration.new
-  end
-
-  def self.define(&block)
-    DSL.run(block)
-  end
-
-  def self.create_view(table_id, *rows)
-    rows = Array.new(rows.flatten(1))
-    query = build_query(rows)
-    client.create_view(table_id, query)
-  end
-
-  def self.client
-    @client ||= BqFactory::Client.new
-  end
-
-  def self.build_query(rows)
-    subqueries = rows.map do |row|
-      %{(SELECT "#{row[:name]}" AS name, #{row[:age]} AS age)}
+  class << self
+    def configure
+      yield configuration if block_given?
+      configuration
     end
-    %{SELECT * FROM #{subqueries.join(', ')}}
-  end
 
-  def self.register_table(name, table)
-    tables.register(name.to_sym, table)
-  end
+    def configuration
+      @configuration ||= Configuration.new
+    end
 
-  def self.tables
-    @tables ||= TableRegistory.new
+    def define(&block)
+      DSL.run(block)
+    end
+
+    def create_view(table_id, *rows)
+      rows = Array.new(rows.flatten(1))
+      query = build_query(rows)
+      client.create_view(table_id, query)
+    end
+
+    def client
+      @client ||= BqFactory::Client.new
+    end
+
+    def build_query(rows)
+      subqueries = rows.map do |row|
+        %{(SELECT "#{row[:name]}" AS name, #{row[:age]} AS age)}
+      end
+      %{SELECT * FROM #{subqueries.join(', ')}}
+    end
+
+    def register_table(name, table)
+      tables.register(name.to_sym, table)
+    end
+
+    def tables
+      @tables ||= TableRegistory.new
+    end
   end
 end
