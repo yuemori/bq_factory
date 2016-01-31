f7f98f3e75675573784bfc65a3f385645da764d8
yuemori <yuemori@aiming-inc.com>
2016-02-01 04:02:06 +0900

    Add simplecov gem

--- bq_factory.gemspec
+++ bq_factory.gemspec
@@ -32,4 +32,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "rubocop"
+  spec.add_development_dependency "simplecov", "~> 0.8.0"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter "/vendor/"
+  add_filter "/spec/"
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'bq_factory'
+
+RSpec.configure do |config|
+  config.order = 'random'
+end
