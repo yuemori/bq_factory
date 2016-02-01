4eed6e5f9d17979b061ab0d2439255a9931db523
yuemori <yuemori@aiming-inc.com>
2016-02-02 00:31:53 +0900

    Refactor client

--- lib/bq_factory/client.rb
+++ lib/bq_factory/client.rb
@@ -1,35 +1,33 @@
 require 'gcloud'
-require 'active_support'
-require 'active_support/core_ext'
 
 module BqFactory
   class Client
     delegate :create_view, to: :dataset
 
     def dataset_create!
-      bigquery.create_dataset(config.dataset_name)
+      bigquery.create_dataset(BqFactory.default_dataset)
     end
 
     def dataset_destroy!
       dataset.delete(force: true)
     end
 
+    def fetch_schema(dataset_name, table_name)
+      dataset(dataset_name).table(table_name).schema["fields"]
+    end
+
     private
 
     def gcloud
-      Gcloud.new config.project_id, config.keyfile_path
+      Gcloud.new BqFactory.project_id, BqFactory.keyfile_path
     end
 
     def bigquery
       gcloud.bigquery
     end
 
-    def dataset
-      bigquery.dataset(config.dataset_name)
-    end
-
-    def config
-      BqFactory.configuration
+    def dataset(dataset_name = BqFactory.default_dataset)
+      bigquery.dataset(dataset_name)
     end
   end
 end
--- spec/bq_factory/client_spec.rb
+++ spec/bq_factory/client_spec.rb
@@ -2,12 +2,8 @@ require 'spec_helper'
 
 describe BqFactory::Client do
   let(:client)   { described_class.new }
-  let(:gcloud)   { double('Gcloud')    }
   let(:bigquery) { double('Bigquery')  }
   let(:dataset)  { double('Dataset')   }
-
-  let(:table_id) { "test" }
-  let(:query)    { "SELECT * FROM test" }
 
   let(:project_id)   { "project_id" }
   let(:keyfile_path) { "/path/to/keyfile.json" }
@@ -17,14 +13,16 @@ describe BqFactory::Client do
     BqFactory.configure do |config|
       config.project_id = project_id
       config.keyfile_path = keyfile_path
-      config.dataset_name = dataset_name
+      config.default_dataset = dataset_name
     end
   end
 
   describe "#create_view" do
+    let(:table_id) { "test" }
+    let(:query)    { "SELECT * FROM test" }
+
     before do
-      allow(client).to receive(:gcloud).and_return(gcloud)
-      allow(gcloud).to receive(:bigquery).and_return(bigquery)
+      allow(client).to receive(:bigquery).and_return(bigquery)
       allow(bigquery).to receive(:dataset).and_return(dataset)
     end
 
@@ -38,8 +36,7 @@ describe BqFactory::Client do
 
   describe "#dataset_create!" do
     before do
-      allow(client).to receive(:gcloud).and_return(gcloud)
-      allow(gcloud).to receive(:bigquery).and_return(bigquery)
+      allow(client).to receive(:bigquery).and_return(bigquery)
     end
 
     subject { client.dataset_create! }
@@ -52,8 +49,7 @@ describe BqFactory::Client do
 
   describe "#dataset_destroy!" do
     before do
-      allow(client).to receive(:gcloud).and_return(gcloud)
-      allow(gcloud).to receive(:bigquery).and_return(bigquery)
+      allow(client).to receive(:bigquery).and_return(bigquery)
       allow(bigquery).to receive(:dataset).with(dataset_name).and_return(dataset)
     end
 
@@ -62,17 +58,6 @@ describe BqFactory::Client do
     it 'should be delegated to the instance of Gcloud' do
       expect(dataset).to receive(:delete).with(force: true)
       expect { subject }.not_to raise_error
-    end
-  end
-
-  describe "settings" do
-    subject { client.send :dataset }
-
-    it 'should be created to the instance of Gcloud with confugiration params' do
-      expect(Gcloud).to receive(:new).with(project_id, keyfile_path).and_return(gcloud)
-      expect(gcloud).to receive(:bigquery).and_return(bigquery)
-      expect(bigquery).to receive(:dataset).with(dataset_name).and_return(dataset)
-      is_expected.to eq dataset
     end
   end
 end
