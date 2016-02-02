c860d474803ca1b9461103b5d35f4e157e8ddd37
yuemori <yuemori@aiming-inc.com>
2016-02-03 04:11:55 +0900

    Fix DSL class

--- lib/bq_factory/dsl.rb
+++ lib/bq_factory/dsl.rb
@@ -1,10 +1,11 @@
 module BqFactory
   class DSL
-    def self.run(&block)
+    def self.run(block)
       new.instance_eval(&block)
     end
 
     def factory(name, options = {})
+      name = name.to_sym
       dataset_name = options.key?(:dataset) ? options[:dataset] : BqFactory.default_dataset
       table_name = options.key?(:table) ? options[:table] : name
       schema = BqFactory.fetch_schema_from_bigquery(dataset_name, table_name)
--- spec/bq_factory/dsl_spec.rb
+++ spec/bq_factory/dsl_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe BqFactory::DSL do
   describe '.run' do
-    subject { described_class.run {} }
+    subject { described_class.run(-> {}) }
 
     it 'should be call instance method' do
       expect_any_instance_of(described_class).to receive(:instance_eval)
