bc1781563b7765aff0c1adb6db7563065ca21ed4
yuemori <yuemori@aiming-inc.com>
2016-02-06 22:23:05 +0900

    Remove vcr, webmock, dotenv gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,22 +8,9 @@ end
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'bq_factory'
 require 'shoulda-matchers'
-require 'dotenv'
-require 'vcr'
-require 'webmock/rspec'
-
-Dotenv.load
 
 RSpec.configure do |config|
   config.order = 'random'
-end
-
-VCR.configure do |config|
-  config.cassette_library_dir = 'spec/vcr'
-  config.hook_into :webmock
-  config.configure_rspec_metadata!
-  config.allow_http_connections_when_no_cassette = true
-  config.filter_sensitive_data("<PROJECT_ID>") { ENV['PROJECT_ID'] }
 end
 
 Shoulda::Matchers.configure do |config|
